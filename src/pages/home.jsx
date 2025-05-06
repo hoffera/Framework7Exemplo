da9f876cd5d6745e14579761bca8d760d8d1ced3
Felipe Hoffmeister Pereira <felipe.hoffmeiste@edu.univali.br>
2025-05-06 14:33:29 -0300

    First commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  Page,
+  Navbar,
+  NavLeft,
+  NavTitle,
+  NavTitleLarge,
+  NavRight,
+  Link,
+  Toolbar,
+  Block,
+  BlockTitle,
+  List,
+  ListItem,
+  Button
+} from 'framework7-react';
+
+const HomePage = () => (
+  <Page name="home">
+    {/* Top Navbar */}
+    <Navbar large sliding={false}>
+      <NavLeft>
+        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="left" />
+      </NavLeft>
+      <NavTitle sliding>Exemplo</NavTitle>
+      <NavRight>
+        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
+      </NavRight>
+      <NavTitleLarge>Exemplo</NavTitleLarge>
+    </Navbar>
+
+    {/* Page content */}
+    <Block>
+      <p>Este é um exemplo de aplicação com layout em abas. O principal objetivo desse tipo de layout com abas é que cada aba contenha uma visualização independente, com seu próprio roteamento e navegação.</p>
+
+      <p>Cada aba/visualização pode ter um layout diferente, um tipo diferente de barra de navegação (dinâmica, fixa ou estática), ou até mesmo não ter barra de navegação, como é o caso desta aba.</p>
+    </Block>
+    <BlockTitle>Navigation</BlockTitle>
+    <List strong inset dividersIos>
+      <ListItem link="/about/" title="Sobre"/>
+      <ListItem link="/form/" title="Formulário "/>
+    </List>
+
+    <BlockTitle>Modals</BlockTitle>
+    <Block className="grid grid-cols-2 grid-gap">
+      <Button fill popupOpen="#my-popup">Popup</Button>
+      <Button fill loginScreenOpen="#my-login-screen">Login Screen</Button>
+    </Block>
+
+    <BlockTitle>Panels</BlockTitle>
+    <Block className="grid grid-cols-2 grid-gap">
+      <Button fill panelOpen="left">Left Panel</Button>
+      <Button fill panelOpen="right">Right Panel</Button>
+    </Block>
+
+    <List strong inset dividersIos>
+      <ListItem
+        title="Dynamic (Component) Route"
+        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
+      />
+      <ListItem
+        title="Default Route (404)"
+        link="/load-something-that-doesnt-exist/"
+      />
+      <ListItem
+        title="Request Data & Load"
+        link="/request-and-load/user/123456/"
+      />
+    </List>
+  </Page>
+);
+export default HomePage;
